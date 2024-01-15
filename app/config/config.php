4bd0b032dd287ec347f5724a74e9d6187eeab390
Shahed Amer <chanyeol6521354@gmail.com>
2024-01-15 14:32:18 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Mvc-2209a-P4');
+define('URLROOT', 'http://be-opdracht5-j2-p3-335476.com');
